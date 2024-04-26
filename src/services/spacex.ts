52fb1f16c020fa8af9121249952d961e21225736
Ivan Urra <ivan.urra.wd@gmail.com>
2024-04-26 07:45:29 +0200

    feat: 404 created & services & [id]

---
+++ src/services/spacex.ts
@@ -0,0 +1,23 @@
+import { type APISpaceXResponse } from "../types/api";
+
+export const getLatestLaunches = async () => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        limit: 15,
+        sort: {
+          date_utc: "asc",
+        },
+      },
+    }),
+  });
+
+  const { docs: launches } = (await res.json()) as APISpaceXResponse;
+
+  return launches;
+};
