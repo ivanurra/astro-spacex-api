bd264f2e3763a4e3ca776dafc3a4fcd0f27ce15e
Ivan Urra <ivan.urra.wd@gmail.com>
2024-04-27 08:14:55 +0200

    feat: getLaunchBy

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,4 +1,14 @@
-import { type APISpaceXResponse } from "../types/api";
+import { type Doc, type APISpaceXResponse } from "../types/api";
+
+export const getLaunchBy = async ({ id }: { id: string }) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+
+  const launch = (await res.json()) as Doc;
+
+  console.log(launch);
+
+  return launch;
+};
 
 export const getLatestLaunches = async () => {
   const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
